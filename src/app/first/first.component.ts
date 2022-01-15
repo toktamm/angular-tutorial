7ba855b0c8db872c7e750dc0386ebf85a9236c8c
Toktam <ttmohebbi@gmail.com>
2022-01-15 18:48:32 -0500

    second way of selecting and showing a component

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-first',
+  selector: '.app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
