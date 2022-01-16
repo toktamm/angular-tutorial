16dadea04cd9a8442d6af5901f45c01e52e9506a
Toktam <ttmohebbi@gmail.com>
2022-01-15 19:14:47 -0500

    use backticks for multiple lines of inline html code

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-first]',
-  template: '<div>Inline Template</div>',
+  template: `<div>
+  Inline Template
+  Second
+  Third
+  </div>`,
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
